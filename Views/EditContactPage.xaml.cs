94b1d119cab7b71bb7a6e54a476dcb9ece0fe559
whereyoucanfindme <anishchy501625@gmail.com>
2024-12-18 21:44:38 +0545

    list page in more better way

--- Views/EditContactPage.xaml.cs
+++ Views/EditContactPage.xaml.cs
@@ -13,17 +13,18 @@ public partial class EditContactPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
+	/*
     private void btnCancel_Clicked(object sender, EventArgs e)
     {
 		Shell.Current.GoToAsync("..");
     }
+	*/
 	public string ContactId
 	{
 		set
 		{
 			contact = ContactRepository.GetContactById(int.Parse(value));
-			lblName.Text = contact.Name;	
+			//lblName.Text = contact.Name;	
 
 		}
 	}
